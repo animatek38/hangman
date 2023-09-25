3cd4f61122bd45f909d4e69df342d25a9398a408
animatek <51943119+animatek38@users.noreply.github.com>
2023-09-25 23:02:38 +0200

    first commit

---
+++ hangman.py
@@ -0,0 +1,64 @@
+import random
+import re
+#put the list countaining a lot of words
+hangman_list = open('wordlist.txt').read().splitlines() # reads the file "wordlist.txt" and put it as a list
+shuffled_word = random.choice(hangman_list) # take at random a word from the list
+shuffled_word = shuffled_word.lower()
+print(shuffled_word)
+hidden_word = ''
+for x in shuffled_word:
+    hidden_word = hidden_word + '_'
+hidden_word = list(hidden_word)
+
+word_lenght = len(shuffled_word) # counts the lenght of the word which has been picked at random
+
+tries = 0 # the number of total guesses from the user, starting at 0
+
+print("\n\nThe word you are looking for is",word_lenght, "characters long\n")
+
+
+def ask():
+    global tries
+    global hidden_word
+    while(tries < 7 ):
+        print(tries, 'while')
+        i = 0
+        wrongLetters = []
+        for x in shuffled_word:
+            print('you have', 7-tries, 'remaining tries')
+            ask_letter = str(input("Enter a letter : ")) # asking the user to guess a leter
+            ask_letter = ask_letter.lower()
+            regex = re.findall("[a-z]", ask_letter)
+            if(len(ask_letter) == 1 and regex):
+                if(ask_letter in shuffled_word):
+                    print('you guessed right')
+                    letterLength = 0
+                    for x in shuffled_word:
+                        if(x==ask_letter):
+                            hidden_word[shuffled_word.find(ask_letter, letterLength)] = ask_letter
+                            letterLength = shuffled_word.find(ask_letter, letterLength) +1
+                else:
+                    wrongLetters.append(ask_letter+ ',')
+                    print("you're gay")
+                    tries += 1
+                    print(tries)
+                    if(tries == 7):
+                        lose()
+                i += 1
+                print("".join(hidden_word))
+                print('wrong letters list :',"".join(wrongLetters))
+                if('_' not in "".join(hidden_word)):
+                    print('you won, now kill yourself')
+                    exit()
+            else:
+                lose()
+    else:
+        print('you dumb as fuck')
+#    if ask_letter: # looking for the input to be no more than 1 letter
+
+def lose():
+    print('u lose kill youself')
+    print('the word was', shuffled_word)
+    exit()
+
+ask()
